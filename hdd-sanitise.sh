5120384e861f77f21d1e4f4b66ece73015752c6b
Tad Vaas <tad@tfix.co.uk>
2023-07-28 07:12:40 +0100

    Update hdd-sanitise.sh

--- hdd-sanitise.sh
+++ hdd-sanitise.sh
@@ -5,8 +5,35 @@ log_dir="/mnt/nfs"
 erase_password="MySecurePassword"
 
 # Fetch all drives excluding root drive and excluding partition numbers
-for device in $(ls /dev/sd* | grep -v "[0-9]$" | grep -v "$root_drive"); do
+for device in $(ls /dev/sd* /dev/md* | grep -v "[0-9]$" | grep -v "$root_drive"); do
     (
+        # Check if the device is part of a RAID array
+        if [[ $device == /dev/md* ]]; then
+            echo "Processing RAID array $device..."
+
+            # Fetch drives in the RAID array
+            raid_devices=$(mdadm --detail "$device" 2>/dev/null | awk '/\/dev/{print $NF}')
+
+            # If the RAID array doesn't exist, skip the device
+            if [ -z "$raid_devices" ]; then
+                echo "Cannot open $device. Skipping..."
+                exit
+            fi
+
+            # Stop the RAID array
+            mdadm --stop "$device"
+
+            # Zero out superblocks of each device in the RAID array
+            for raid_device in $raid_devices; do
+                echo "Zeroing out superblock of $raid_device..."
+                mdadm --zero-superblock "$raid_device"
+            done
+
+            # Skip further processing for the RAID device
+            echo "Skipping further processing for RAID array $device..."
+            exit
+        fi
+
         # Fetch drive details
         serial=$(smartctl -i $device | grep -i 'Serial number:' | awk '{print $3}')
         model=$(smartctl -i $device | grep -i 'Device Model:' | awk '{print $3}')
@@ -15,20 +42,41 @@ for device in $(ls /dev/sd* | grep -v "[0-9]$" | grep -v "$root_drive"); do
         # If any of these details are empty, skip the device
         if [[ -z "$serial" ]] || [[ -z "$model" ]] || [[ -z "$size" ]]; then
             echo "Skipping $device due to missing details."
-            continue
+            exit
         fi
 
         # Print drive details
         echo "Processing $device with serial number $serial, model $model, and size $size..."
 
-        # Perform SMART test
-        echo "Start SMART test on $device..."
-        smart_data=$(smartctl -A "$device")
-        
-        # Check if SMART data was successfully accessed
-        if [[ -z "$smart_data" ]]; then
-            echo "Failed to fetch SMART data for $device. Skipping..."
-            continue
+        # Abort any ongoing SMART test
+        smartctl -X "$device"
+
+        # Perform SMART self-test and check result
+        smartctl -t short "$device"
+        sleep 2
+        smart_status=$(smartctl -H "$device")
+        if echo "$smart_status" | grep -q "PASSED"; then
+            echo "SMART self-test for $device passed."
+        else
+            echo "SMART self-test for $device failed. Exiting..."
+            exit
+        fi
+
+        # Print drive details
+        echo "Processing $device with serial number $serial, model $model, and size $size..."
+
+        # Abort any ongoing SMART test
+        smartctl -X "$device"
+
+        # Perform SMART self-test and check result
+        smartctl -t short "$device"
+        sleep 2
+        smart_status=$(smartctl -H "$device")
+        if echo "$smart_status" | grep -q "PASSED"; then
+            echo "SMART self-test for $device passed."
+        else
+            echo "SMART self-test for $device failed. Exiting..."
+            exit
         fi
 
         # Perform badblocks test
@@ -49,12 +97,23 @@ for device in $(ls /dev/sd* | grep -v "[0-9]$" | grep -v "$root_drive"); do
             # Issue the secure erase command
             hdparm --security-erase $erase_password "$device"
         else
+            # Error handling for dd command
+            set -e
+            trap 'echo "dd command failed with exit code $?"' ERR
             # Get disk size in bytes for pv
             disk_size=$(blockdev --getsize64 "$device")
-
             # Overwrite drive once with random data
             echo "Start overwriting $device with random data..."
             dd if=/dev/urandom | pv -s $disk_size | dd of="$device" bs=1M iflag=fullblock
+            trap - ERR
+            set +e
+        fi
+
+        # Verification of the erase
+        echo "Verifying erase..."
+        dd if="$device" bs=1M count=100 | hexdump -C | grep -qv '00 00 00 00'
+        if [ $? -ne 1 ]; then
+            echo "Erase verification failed for $device. Please check."
         fi
 
         # Create a certificate text file
@@ -63,7 +122,7 @@ for device in $(ls /dev/sd* | grep -v "[0-9]$" | grep -v "$root_drive"); do
         echo "Model: $model" >> "$log_dir/certificate_$serial.txt"
         echo "Disk Size: $size" >> "$log_dir/certificate_$serial.txt"
         echo "Relevant SMART Data:" >> "$log_dir/certificate_$serial.txt"
-        echo "$smart_data" >> "$log_dir/certificate_$serial.txt"
+        echo "$smart_status" >> "$log_dir/certificate_$serial.txt"
         echo "Badblocks Test Result:" >> "$log_dir/certificate_$serial.txt"
         echo "$badblocks_output" >> "$log_dir/certificate_$serial.txt"
         echo "Erasure was successful." >> "$log_dir/certificate_$serial.txt"
